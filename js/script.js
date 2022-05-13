16b53b0e1f550655a883a38f60829538cb3ef7fd
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-13 15:11:05 +0200

    aggiunta seconda parte js e fix generali

--- js/script.js
+++ js/script.js
@@ -15,8 +15,37 @@ const teamMembers = [
         fullName: "Wayne Barnett",
         memberRole: "Founder & CEO"
     },
+    {
+        profilePic: "img/wayne-barnett-founder-ceo.jpg",
+        fullName: "Wayne Barnett",
+        memberRole: "Founder & CEO"
+    },
+    {
+        profilePic: "img/wayne-barnett-founder-ceo.jpg",
+        fullName: "Wayne Barnett",
+        memberRole: "Founder & CEO"
+    },
+    {
+        profilePic: "img/wayne-barnett-founder-ceo.jpg",
+        fullName: "Wayne Barnett",
+        memberRole: "Founder & CEO"
+    },
 ];
 
+const card = document.querySelector(".team-container");
+
 for (let i = 0 ; i < teamMembers.length ; i++) {
-    console.log(teamMembers[i]);
+    const member = `
+        <div class="team-card">
+            <div class="card-image">
+                <img src="${teamMembers[i].profilePic}" alt="Wayne Barnett"/>
+            </div>
+            <div class="card-text">
+                <h3>${teamMembers[i].fullName}</h3>
+                <p>${teamMembers[i].memberRole}</p>
+            </div>
+        </div>    
+    `;
+    card.innerHTML += member;
 }
+console.log(card);
