8986d8bd763f5f0b6a3b3d9795a389ca8ac5747a
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-13 14:49:53 +0200

    aggiunta prima parte js e aggiunta commenti

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Viene fornito un layout di base in cui è presente una card di esempio inserita staticamente nell’html. Questa card serve solo come timbro del markup necessario per ottenere il risultato finale e dovrà quindi essere rimossa dall’html.
-// Aggiungere un file js in cui definire un array di oggetti che rappresentano i membri del team. Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
-// Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell'html, stampare dinamicamente una card per ogni membro del team.
-// BONUS:
-// Utilizzare gli input presenti nella pagina per permettere all'utente di aggiungere nuovi membri del team: cliccando sul pulsante "add" viene creato un nuovo oggetto, il quale viene inserito nell'array iniziale e viene stampata una nuova card con tutte le informazioni inserite dall'utente.
+//------ FUNCTIONS ------//
+
+
+
+//------ MAIN ------//
+// 1. creo un array che contenga come oggetto i componeti del team
+// 2. rimuovo dall'html la card contenente il primo componente del team
+// 3. faccio ciclare (con un ciclo for per gli array) i componenti del team aggiunti tramite js
+// 4. sostituisco il markup html con quello javascript tramite il template literal
+// BONUS: rendere funzionante il form presente in pagina
+
+const teamMembers = [
+    {
+        profilePic: "img/wayne-barnett-founder-ceo.jpg",
+        fullName: "Wayne Barnett",
+        memberRole: "Founder & CEO"
+    },
+];
+
+for (let i = 0 ; i < teamMembers.length ; i++) {
+    console.log(teamMembers[i]);
+}
